84e5d62a066128f830dc74cf80ef4c03f4fdfa1b
Shrinivass Arunachalam Balasubramanian <shrinivassab@gmail.com>
2025-01-05 22:53:02 -0500

    on push strategy

---
+++ change-detection-strategy-on-push.ts
@@ -0,0 +1,11 @@
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class UserProfileComponent {
+  @Input() user: any;
+}
